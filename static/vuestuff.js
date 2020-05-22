91c7d44b2c2a5ba69e5b32028681ce05d2413ed3
hooty <hooty@hooty.com>
2020-05-21 23:33:17 -0500

    added vuestuff.js

---
+++ static/vuestuff.js
@@ -0,0 +1,83 @@
+
+// ! VUE STUFF ------------------------------------------------------------------->
+
+
+Vue.component('custom_row', {
+	props: ['match_prop'],
+	methods: {
+	},	
+	template:  ``
+})
+
+var vm = new Vue({
+	el: "#vueapp",
+	data: {
+		match_data: [],
+		strTeamRoster: '',
+	},
+	created: function () {
+	},
+	methods: {
+		getMatchData: function (matches_array) {
+			this.match_data = matches_array;
+
+			//console.log('getMatchData()');
+			console.dir(this.match_data);
+        },
+        resolveMatchType: function (match_type) {
+			if (match_type == 'competitive') {
+				return 'Ranked'
+			} else {
+				return 'Unranked'
+			}
+        },
+        resolveKnocks: function (mode, knocks) {
+            if (mode == 'solo' || mode == 'solo-fpp') {
+                return '-';
+            }
+            else {
+                return knocks;
+            }
+
+        },
+		printRoster: function (matchRoster, player_name) {
+			
+			var strRoster = '';
+
+			for (let i = 0; i < matchRoster.length; i++){
+				// strRoster += matchRoster[i].name;
+
+				// if (i + 1 < matchRoster.length) {
+				// 	strRoster += ',';
+                // }
+
+
+                if (matchRoster[i].name != player_name) {
+                    strRoster += ', ' + matchRoster[i].name;
+                }
+
+			}
+        
+            if (strRoster != '') {
+                strRoster = player_name + strRoster;
+            }
+            else {
+                strRoster = player_name;
+            }
+
+            if (matchRoster.length == 1){
+                strRoster = '-';
+            }
+
+			return strRoster;
+		},
+		analyzeMatch: function (_matchID) {
+			//console.log('analyzeMatch() -> ' + _matchID);
+
+			GetTelemetry(_matchID);
+
+
+		}
+    },
+    
+})
