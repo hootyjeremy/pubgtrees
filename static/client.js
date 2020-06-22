d63c1bf3f4569ba056ecc61d243c4a9278a40a12
hooty <hooty@hooty.com>
2020-06-21 23:39:43 -0500

    test

--- static/client.js
+++ static/client.js
@@ -12,7 +12,7 @@ let defaultPlayer		= 'hooty__';
 const blTestingVersion 	= !true;
 
 if (!blTestingVersion) {
-	hooty_server_url 	= 'https://https://hooty-pubg01.herokuapp.com/';
+	hooty_server_url 	= 'https://hooty-pubg01.herokuapp.com/';
 	defaultPlayer 		= '';
 }
 // --------------------------------------------------------->
