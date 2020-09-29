a256edaef3625289de051ef35c2ab6f74820ac0c
hooty <hooty@hooty.com>
2020-09-29 17:54:04 -0500

    Revert "test"
    This reverts commit 313919c770916d55a95e2f2da01f33f1d54a790f.

--- static/client.js
+++ static/client.js
@@ -16,7 +16,7 @@ let hooty_server_url 	= 'http://localhost:3000';
 
 // Deploy/Testing Version...
 let   version 			= '0.049'
-const blTestingVersion 	= true;
+const blTestingVersion 	= !true;
 
 
 if (!blTestingVersion) {
