bbbeaa2b4a2b44db39db69543fdc0d29d3d56639
hooty <hooty@hooty.com>
2020-12-13 21:34:23 -0600

    test

--- static/client.js
+++ static/client.js
@@ -15,7 +15,7 @@ let hooty_server_url 	= 'http://localhost:3000';
 // --------------------------------------------------------->
 
 // Deploy/Testing Version...
-let   version 			= '0.055'
+let   version 			= '0.056'
 const blTestingVersion 	= !true;
 
 
