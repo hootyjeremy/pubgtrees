0f82b0003d2097c4136c0037f2298d3f65b60faa
hooty <hooty@hooty.com>
2021-12-13 16:04:40 -0600

    panzerfaust explosion

--- hooty_modules/hf_server.js
+++ hooty_modules/hf_server.js
@@ -169,6 +169,9 @@ var _damageTypeCategory = {
     "Damage_KillTruckHit": "Kill Truck",
     "Damage_KillTruckTurret": "Kill Truck Turret",
     "Damage_Explosion_LootTruck": "Loot Truck Explosion",
+
+    "Panzerfaust Explosion": "Panzerfaust",
+    
 }
 
 //https://github.com/pubg/api-assets/blob/master/dictionaries/telemetry/damageCauserName.json
