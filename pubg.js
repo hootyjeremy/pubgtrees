7e35f223dd88dece58d883941cf483198bb5defd
hooty <hooty@hooty.com>
2022-01-21 02:04:08 -0600

    log5

--- pubg.js
+++ pubg.js
@@ -171,8 +171,8 @@ app.get('/getplayermatches', async (req, res) => {
         console.log('searchDirection: ' + searchDirection);
     }
 
-    console.log('req.ip --> ' + req.ip)
-    console.log(`req.headers['x-forwarded-for'] --> ` + req.headers['x-forwarded-for'])
+    //console.log('req.ip --> ' + req.ip)
+    //console.log(`req.headers['x-forwarded-for'] --> ` + req.headers['x-forwarded-for'])
     
 
     // console.log('request ip: ' + req.ip);
